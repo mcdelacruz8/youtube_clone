b5f538d38b01dc7705f75238983581a5d0d081b1
mcdelacruz8 <88753390+mcdelacruz8@users.noreply.github.com>
2022-12-19 16:36:04 -0800

    Add files via upload

--- components/ChannelDetail.jsx
+++ components/ChannelDetail.jsx
@@ -17,7 +17,7 @@ const ChannelDetail = () => {
 
       setChannelDetail(data?.items[0]);
 
-      const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
+      const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`);
 
       setVideos(videosData?.items);
     };
@@ -33,7 +33,7 @@ const ChannelDetail = () => {
           background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
           zIndex: 10,
         }} />
-        <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
       </Box>
       <Box p={2} display="flex">
         <Box sx={{ mr: { sm: '100px' } }} />
